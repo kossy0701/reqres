b9abe49e367d73eca67e3cdf1bbfbba01da53e8d
kossy0701 <0421kossy0421@gmail.com>
2020-12-05 20:52:53 +0900

    create UserListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListItemComponent } from './user-list-item/user-list-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListItemComponent
+    UserListItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
