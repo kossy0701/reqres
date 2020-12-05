b14f9fe4e702170b1e6b0d8bc958c1a51e3a912c
kossy0701 <0421kossy0421@gmail.com>
2020-12-05 20:16:45 +0900

    create fetch user function for reqres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'reqres';
+export class AppComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<{ data: User[] }>('https://reqres.in/api/users')
+      .subscribe((resp) => {
+        this.users = resp.data;
+      });
+  }
 }
